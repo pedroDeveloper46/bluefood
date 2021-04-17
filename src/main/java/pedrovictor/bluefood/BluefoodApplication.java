c17ce40b0ea3a165ff983b7f59c8116da0f82a4d
Pedro Victor <pedro46tr@gmail.com>
2021-04-16 22:21:05 -0300

    Projeto Spring

---
+++ src/main/java/pedrovictor/bluefood/BluefoodApplication.java
@@ -0,0 +1,14 @@
+package pedrovictor.bluefood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class BluefoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BluefoodApplication.class, args);
+	}
+
+}
